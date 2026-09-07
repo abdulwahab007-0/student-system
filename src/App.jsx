2eaf30635d251f076a85ac031e4cd5af49292360
abdulwahab007-0 <abdulwahabsani08@gmail.com>
2026-09-07 18:36:22 +0500

    fix: add 15 missing React Router routes for sidebar nav pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import { useAuth } from "./context/AuthContext";
 import Approvals from "./pages/Approvals";
+import ApproveAttendance from "./pages/ApproveAttendance";
+import AttendanceGeofence from "./pages/AttendanceGeofence";
+import AttendanceReport from "./pages/AttendanceReport";
+import Chat from "./pages/Chat";
 import Classes from "./pages/Classes";
+import ClassSchedule from "./pages/ClassSchedule";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import ManageCR from "./pages/ManageCR";
+import ManageSystemAdmin from "./pages/ManageSystemAdmin";
+import ManageTeacher from "./pages/ManageTeacher";
 import ManageUsers from "./pages/ManageUsers";
 import Marks from "./pages/Marks";
+import MarkAttendance from "./pages/MarkAttendance";
+import MyAttendance from "./pages/MyAttendance";
+import MyCard from "./pages/MyCard";
+import MyTeacherCard from "./pages/MyTeacherCard";
 import Register from "./pages/Register";
+import StudentAttendance from "./pages/StudentAttendance";
 import Students from "./pages/Students";
+import StudentCards from "./pages/StudentCards";
 import Subjects from "./pages/Subjects";
+import TeacherCards from "./pages/TeacherCards";
 import Teachers from "./pages/Teachers";
+import TodoList from "./pages/TodoList";
 import UserRights from "./pages/UserRights";
 
 function PrivateRoute({ children, allowed, permission }) {
@@ -89,6 +104,107 @@ function App() {
                         </PrivateRoute>
                     }
                 />
+                {/* Attendance */}
+                <Route
+                    path="mark-attendance"
+                    element={
+                        <PrivateRoute permission="mark_attendance">
+                            <MarkAttendance />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="student-attendance"
+                    element={
+                        <PrivateRoute permission="view_attendance_report">
+                            <StudentAttendance />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="my-attendance"
+                    element={
+                        <PrivateRoute permission="view_own_attendance">
+                            <MyAttendance />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="approve-attendance"
+                    element={
+                        <PrivateRoute permission="approve_attendance">
+                            <ApproveAttendance />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="attendance-report"
+                    element={
+                        <PrivateRoute permission="view_attendance_report">
+                            <AttendanceReport />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="attendance-areas"
+                    element={
+                        <PrivateRoute permission="manage_geofences">
+                            <AttendanceGeofence />
+                        </PrivateRoute>
+                    }
+                />
+                {/* Chat */}
+                <Route
+                    path="chat"
+                    element={
+                        <PrivateRoute permission="use_chat">
+                            <Chat />
+                        </PrivateRoute>
+                    }
+                />
+                {/* Class Schedule */}
+                <Route
+                    path="class-schedule"
+                    element={
+                        <PrivateRoute permission="view_class_schedule">
+                            <ClassSchedule />
+                        </PrivateRoute>
+                    }
+                />
+                {/* ID Cards */}
+                <Route
+                    path="student-cards"
+                    element={
+                        <PrivateRoute permission="view_student_cards">
+                            <StudentCards />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="teacher-cards"
+                    element={
+                        <PrivateRoute permission="view_teacher_cards">
+                            <TeacherCards />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="my-card"
+                    element={
+                        <PrivateRoute permission="view_own_card">
+                            <MyCard />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="my-teacher-card"
+                    element={
+                        <PrivateRoute permission="view_own_teacher_card">
+                            <MyTeacherCard />
+                        </PrivateRoute>
+                    }
+                />
+                {/* System */}
                 <Route
                     path="approvals"
                     element={
@@ -114,6 +230,31 @@ function App() {
                     }
                 />
                 {isSuperAdmin && <Route path="manage-cr" element={<ManageCR />} />}
+                <Route
+                    path="manage-teachers"
+                    element={
+                        <PrivateRoute allowed={["super_admin"]}>
+                            <ManageTeacher />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="manage-admins"
+                    element={
+                        <PrivateRoute allowed={["super_admin"]}>
+                            <ManageSystemAdmin />
+                        </PrivateRoute>
+                    }
+                />
+                {/* Todo */}
+                <Route
+                    path="todo-list"
+                    element={
+                        <PrivateRoute permission="view_todo_list">
+                            <TodoList />
+                        </PrivateRoute>
+                    }
+                />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Route>
         </Routes>
